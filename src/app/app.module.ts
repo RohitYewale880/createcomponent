85fd417a4bc1519c82cb63c381c29bf9236b805c
RohitYewale880 <rohityewale2020@gmail.com>
2026-04-13 11:49:17 +0530

    creating component manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from 'src/shared/component/product/product.component';
+import { TestComponent } from 'src/shared/component/test/test.component';
+import { UserComponent } from 'src/shared/component/user/user.component';
+import { PostComponent } from 'src/shared/component/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    TestComponent,
+    UserComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
